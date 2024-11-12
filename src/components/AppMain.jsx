217337134f55140589e48fddc7c8f2fc5894e4de
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-12 16:07:24 +0100

    add: main with style and components

---
+++ src/components/AppMain.jsx
@@ -0,0 +1,12 @@
+import BlogCard from "./BlogCards/BlogCards"
+export default function AppMain() {
+    return (
+        <main>
+            <section className="mainCards">
+                <BlogCard />
+            </section>
+
+        </main>
+
+    )
+}
